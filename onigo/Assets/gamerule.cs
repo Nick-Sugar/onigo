9bd5f3c43ae76992f182bd7493a4a47897896640
nick-sugar <nicksugar0141@gmail.com>
2020-08-28 14:02:24 +0900

    test

--- onigo/Assets/gamerule.cs
+++ onigo/Assets/gamerule.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
@@ -8,7 +9,7 @@ using Unity.MLAgents.Sensors;
 [RequireComponent(typeof(Agent))]
 public class gamerule : Agent
 {
-    float speed = 1.0f;
+    float speed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,40 +19,49 @@ public class gamerule : Agent
     // Update is called once per frame
     void Update()
     {
+        if (Input.anyKey)
+        {
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(code))
+                {
+                    // 入力されたキー名を表示
+                    Debug.Log(code.ToString());
+                }
+            }
+        }
 
-
-        Vector3 move = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
         {
-            move = new Vector3(0, 0, 0.5f);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            move = new Vector3(0, 0, -0.5f);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * speed, ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            move = new Vector3(-0.5f, 0, 0);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(-transform.right * speed, ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            move = new Vector3(0.5f, 0, 0);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(transform.right * speed, ForceMode.Impulse);
         }
 
 
         Vector3 rotation = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.Q))
         {
-            move = new Vector3(-90f,0,0);
+            this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 1, 0)*2.0f;
         }
         if (Input.GetKey(KeyCode.Z))
         {
-            move = new Vector3(90f,0,0);
+            this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, -1, 0) * 2.0f;
         }
 
-        this.gameObject.GetComponent<Rigidbody>().angularVelocity = rotation;
         
-        this.gameObject.GetComponent<Rigidbody>().AddForce(move * speed, ForceMode.Impulse);
+        //this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 1, 0);
+
     }
 
     public override void OnEpisodeBegin()
@@ -62,11 +72,14 @@ public class gamerule : Agent
     }
     public override void CollectObservations(VectorSensor sensor)
     {
-
+        sensor.AddObservation(this.transform.position);
+        sensor.AddObservation(this.transform.rotation);
     }
     public override void OnActionReceived(float[] vectorAction)
     {
-        Vector3 position = new Vector3(vectorAction[0], vectorAction[1], vectorAction[2]);
-        this.gameObject.GetComponent<Rigidbody>().AddForce(position, ForceMode.Impulse);
+        this.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * vectorAction[0], ForceMode.Impulse);
+        this.gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * vectorAction[1], ForceMode.Impulse);
+        this.gameObject.GetComponent<Rigidbody>().AddForce(-transform.right * vectorAction[2], ForceMode.Impulse);
+        this.gameObject.GetComponent<Rigidbody>().AddForce(transform.right * vectorAction[3], ForceMode.Impulse);
     }
 }
