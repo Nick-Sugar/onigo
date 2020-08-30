d5f38f1198ea8f4b9ce58e4c74ee20029e45be16
nick-sugar <nicksugar0141@gmail.com>
2020-08-30 14:49:52 +0900

    test

--- onigo/Assets/gamerule.cs
+++ onigo/Assets/gamerule.cs
@@ -6,10 +6,14 @@ using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 
 [RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(Agent))]
 public class gamerule : Agent
 {
     float speed = 0.5f;
+
+    public Transform Target;
+
+    public worldtimer timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -56,7 +60,7 @@ public class gamerule : Agent
         }
         if (Input.GetKey(KeyCode.Z))
         {
-            this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, -1, 0) * 2.0f;
+            this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 1, 0) * -2.0f;
         }
 
         
@@ -68,12 +72,13 @@ public class gamerule : Agent
     {
         this.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        this.transform.localPosition = new Vector3(0, 0.5f, 0);
+        this.transform.localPosition = new Vector3(UnityEngine.Random.value * 8 - 4, 0.5f, UnityEngine.Random.value * 8 - 4);
     }
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(this.transform.position);
         sensor.AddObservation(this.transform.rotation);
+        sensor.AddObservation(Target);
     }
     public override void OnActionReceived(float[] vectorAction)
     {
@@ -81,5 +86,23 @@ public class gamerule : Agent
         this.gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * vectorAction[1], ForceMode.Impulse);
         this.gameObject.GetComponent<Rigidbody>().AddForce(-transform.right * vectorAction[2], ForceMode.Impulse);
         this.gameObject.GetComponent<Rigidbody>().AddForce(transform.right * vectorAction[3], ForceMode.Impulse);
+        this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, vectorAction[4], 0) * -2.0f;
+        this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, vectorAction[5], 0) * -2.0f;
+
+
+        float enemyspace = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+
+        if (enemyspace < 1.0f)
+        {
+            SetReward(0.5f);
+            EndEpisode();
+        }
+
+        if (timer.Reset)
+        {
+            SetReward(-0.5f);
+            EndEpisode();
+        }
     }
 }
---
+++ onigo/Assets/worldtimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldtimer : MonoBehaviour
+{
+
+    public float time;
+
+    public bool Reset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            Reset = true;
+        }
+        else
+        {
+            Reset = false;
+        }
+    }
+
+}
